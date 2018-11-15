7de84bffeb23abc42f9d736f1fc5e64ff6e69b15
remy <remy.guyonneau@univ-angers.fr>
2018-11-15 14:35:40 +0100

    script correction

--- scripts/init_scripts.sh
+++ scripts/init_scripts.sh
@@ -2,11 +2,13 @@
 # this script is called "init_scripts.sh"
 # the idea is to put the scripts in the folder /home/pi/bin
 
-if [ ! -d /home/pi/bin]
+if [ ! -d /home/pi/bin ];
+then
     mkdir /home/pi/bin
 fi
 
-if [ ! -d /home/pi/logs]
+if [ ! -d /home/pi/logs ];
+then
     mkdir /home/pi/logs
 fi
 
--- scripts/update_scripts.sh
+++ scripts/update_scripts.sh
@@ -3,6 +3,6 @@
 # The idea is to copy the scripts into the github repository in oder to update them if needed
 # note that it is assumed that the istia_rover github repositirory is at /home/pi/ros_catkin_ws/src/istia_rover !
 
-yes | cp -f /home/pi/bin/* /home/pi/ros_catkin_ws/src/istia_rover/scritps/*
+yes | cp -f /home/pi/bin/* /home/pi/ros_catkin_ws/src/istia_rover/scripts/
 
 
