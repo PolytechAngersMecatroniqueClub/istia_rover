c7e4048ae0485aae7be22a0fe42e80bf8a10aff8
remy <remy.guyonneau@univ-angers.fr>
2018-12-18 12:11:35 +0100

    updating the scripts update and init

--- scripts/init_scripts.sh
+++ scripts/init_scripts.sh
@@ -2,18 +2,38 @@
 # this script is called "init_scripts.sh"
 # the idea is to put the scripts in the folder /home/pi/bin
 
-if [ ! -d /home/pi/bin ];
+
+
+
+read -p "This will copy the scripts from the git repository to the bin directory, do you want to continue ? (y/n)" input
+
+if [ $input = "y" ] 
+then 
+    if [ ! -d /home/pi/bin ];
+    then
+        mkdir /home/pi/bin
+        echo "The bin directory does not exists, it has been created (/home/pi/bin)"
+    fi
+
+    if [ ! -d /home/pi/logs ];
+    then
+        mkdir /home/pi/logs
+        echo "The logs directory does not exists, it has been created (/home/pi/logs)"
+    fi
+
+    yes | cp -f /ros_catkin_ws/scripts/* /home/pi/bin/
+    echo "Scripts copied from git to bin"
+    # the init_scripts should not be done from the /home/pi/bin directory
+    # this is done to avoid unwanted overwritting
+    rm /home/pi/bin/init_scripts.sh
+    echo "the file init_scripts has been removed from the bin directory because it should only be execute in the git directory"
+elif [ $input = "n" ]
 then
-    mkdir /home/pi/bin
+    echo "Abord!"
+else
+    echo "the choice \"$input\" is not valid"
 fi
 
-if [ ! -d /home/pi/logs ];
-then
-    mkdir /home/pi/logs
-fi
 
-mv * /home/pi/bin/
 
-# the init_scripts should not be done from the /home/pi/bin directory
-# this is done to avoid unwanted overwritting
-rm /home/pi/bin/init_scripts
+
--- scripts/update_scripts.sh
+++ scripts/update_scripts.sh
@@ -3,6 +3,18 @@
 # The idea is to copy the scripts into the github repository in oder to update them if needed
 # note that it is assumed that the istia_rover github repositirory is at /home/pi/ros_catkin_ws/src/istia_rover !
 
-yes | cp -f /home/pi/bin/* /home/pi/ros_catkin_ws/src/istia_rover/scripts/
+
+read -p "This will copy the scripts from the bin directory to git repository, do you want to continue ? (y/n)" input
+
+if [ $input = "y" ] 
+then 
+    yes | cp -f /home/pi/bin/* /home/pi/ros_catkin_ws/src/istia_rover/scripts/
+    echo "Scripts copied from bin to git"
+elif [ $input = "n" ]
+then
+    echo "Abord!"
+else
+    echo "the choice \"$input\" is not valid"
+fi
 
 
