fd30309593fefa3138c81ae1808bf4f226c81a3c
cerema <remy.guyonneau@univ-angers.fr>
2019-05-09 15:47:14 +0200

    adding servo

--- src/motor_hat_twist_node.cpp
+++ src/motor_hat_twist_node.cpp
@@ -12,10 +12,8 @@ from matpalm (https://github.com/matpalm/ros-motorhat-node).
 using namespace std;
 
 string cst_topic_name;
-int cst_motor_bot_left;
-int cst_motor_bot_right;
-int cst_motor_front_left;
-int cst_motor_front_right;
+int cst_motor_drive;
+int cst_motor_steer;
 int cst_max_speed;
 double cst_max_twist;
 
@@ -23,7 +21,8 @@ double cst_max_twist;
 // ------------------------------
 // HAT control registers and data
 
-const int I2C_ADDR = 0x60;
+const int I2C_ADDR_DRIVE = 0x60;
+const int I2C_ADDR_STEER = 0x40;
 
 const int MODE1 = 0x00;
 const int MODE2 = 0x01;
@@ -44,41 +43,58 @@ const int OUTDRV = 0x04;
 // --------------------------------------------------
 // mappings from motorN to specific control registers
 //                     m0  m1  m2  m3
-const int pwmPin[] = {  8, 13,  2,  7 };
-const int in1Pin[] = { 10, 11,  4,  5 };
-const int in2Pin[] = {  9, 12,  3,  6 };
+const int pwmPin_drive = 8;
+const int in1Pin_drive = 10;
+const int in2Pin_drive = 9;
+
+const int pwmPin_steer = 15;
 
 // -------------------------
 // i2c and pwm setting utils
 
-int fd = 0;
+int fd_drive = 0;
+int fd_steer = 0;
+
 void i2c_setup() {
-    fd = wiringPiI2CSetup(I2C_ADDR);
-    if (fd == -1)
-        ROS_ERROR("Cant find I2C device with addr %d", I2C_ADDR);
+    fd_drive = wiringPiI2CSetup(I2C_ADDR_DRIVE);
+    if (fd_drive == -1)
+        ROS_ERROR("Cant find I2C device with addr %d (Drive motor)", I2C_ADDR_DRIVE);
+    else ROS_INFO("fd_drive %d (i2C)", fd_drive);
+
+    fd_steer = wiringPiI2CSetup(I2C_ADDR_STEER);
+    if (fd_steer == -1)
+        ROS_ERROR("Cant find I2C device with addr %d (Steer motor)", I2C_ADDR_STEER);
+    else ROS_INFO("fd_steer %d (i2C)", fd_steer);
 }
 
-void write(int reg, int data) {
+void write(int fd, int reg, int data) {
     wiringPiI2CWriteReg8(fd, reg, data);
 }
 
-int read(int reg) {
+int read(int fd, int reg) {
     int val = wiringPiI2CReadReg8(fd, reg);
     return val;
 }
 
-void setPWM(int channel, int on, int off) {
-    write(LED0_ON_L+4*channel, on & 0xFF);
-    write(LED0_ON_H+4*channel, on >> 8);
-    write(LED0_OFF_L+4*channel, off & 0xFF);
-    write(LED0_OFF_H+4*channel, off >> 8);
+void setPWM(int fd, int channel, int on, int off) {
+    write(fd, LED0_ON_L+4*channel, on & 0xFF);
+    write(fd, LED0_ON_H+4*channel, on >> 8);
+    write(fd, LED0_OFF_L+4*channel, off & 0xFF);
+    write(fd, LED0_OFF_H+4*channel, off >> 8);
 }
 
-void setPin(int pin, int val) {
+void setPWMSteer(int led, int on, int off) {
+    write(fd_steer, LED0_ON_L+4*(led-1), on & 0xFF);
+    write(fd_steer, LED0_ON_H+4*(led-1), on >> 8);
+    write(fd_steer, LED0_OFF_L+4*(led-1), off & 0xFF);
+    write(fd_steer, LED0_OFF_H+4*(led-1), off >> 8);
+}
+
+void setPin(int fd, int pin, int val) {
     if (val == 0)
-        setPWM(pin, 0, 4096);
+        setPWM(fd, pin, 0, 4096);
     else if (val == 1)
-        setPWM(pin, 4096, 0);
+        setPWM(fd, pin, 4096, 0);
     else
         cerr << "invalid val for setPin(" << pin << "," << val << ")" << endl;
 }
@@ -90,46 +106,90 @@ void init_hat() {
     // This is just cut and paste (including sleeps)
     // See original for some more explaination....
     i2c_setup();
-    write(ALL_LED_ON_L, 0);
-    write(ALL_LED_ON_H, 0);
-    write(ALL_LED_OFF_L, 0);
-    write(ALL_LED_OFF_H, 0);
-    write(MODE2, OUTDRV);
-    write(MODE1, ALLCALL);
+
+    //init the driver hat
+    write(fd_drive, ALL_LED_ON_L, 0);
+    write(fd_drive, ALL_LED_ON_H, 0);
+    write(fd_drive, ALL_LED_OFF_L, 0);
+    write(fd_drive, ALL_LED_OFF_H, 0);
+    write(fd_drive, MODE2, OUTDRV);
+    write(fd_drive, MODE1, ALLCALL);
     usleep(5000);
-    int mode1 = read(MODE1);
-    mode1 &= ~SLEEP;
-    write(MODE1, mode1);
+    int mode1_drive = read(fd_drive, MODE1);
+    mode1_drive &= ~SLEEP;
+    write(fd_drive, MODE1, mode1_drive);
     usleep(5000);
+
+    /*//init the steer hat (from PCA9685 documentation - github)
+    write(fd_steer, ALL_LED_ON_L, 0);
+    write(fd_steer, ALL_LED_ON_H, 0);
+    write(fd_steer, ALL_LED_OFF_L, 0);
+    write(fd_steer, ALL_LED_OFF_H, 0);
+    write(fd_steer, MODE1, ALLCALL);
+    write(fd_steer, MODE2, OUTDRV);
+    usleep(5000);
+    int mode1_steer = read(fd_steer, MODE1);
+    mode1_steer &= ~SLEEP;
+    write(fd_steer, MODE1, mode1_steer);
+    usleep(5000);*/
+
+    // reset
+    write(fd_steer, MODE1, 0x00); // Normal mode
+    write(fd_steer, MODE2, 0x04); // totem pole
+    usleep(5000);
+    // set frequency
+    int freq = 50; // between 40Hz and 1000Hz considering a 25MHz oscillator
+    float prescaleval = 25000000.0;
+    prescaleval /= 4096.0;
+    prescaleval /= freq;
+    prescaleval -= 1.0;
+    write(fd_steer, MODE1, 0x10); // Sleep
+    write(fd_steer, PRESCALE, floor(prescaleval));
+    write(fd_steer, MODE1, 0x80); // restart
+    write(fd_steer, MODE2, 0x04); // totem pole, default
 }
 
-void setPWMFreq(int freq) {
+void setPWMFreq(int fd, int freq) {
     float prescaleval = 25000000.0;
     prescaleval /= 4096.0;
     prescaleval /= freq;
     prescaleval -= 1.0;
     float prescale = floor(prescaleval + 0.5);
-    int old_mode = read(MODE1);
+    int old_mode = read(fd, MODE1);
     int new_mode = (old_mode & 0x7F) | 0x10;
-    write(MODE1, new_mode);
-    write(PRESCALE, int(floor(prescale)));
-    write(MODE1, old_mode);
+    write(fd, MODE1, new_mode);
+    write(fd, PRESCALE, int(floor(prescale)));
+    write(fd, MODE1, old_mode);
     usleep(5000);
-    write(MODE1, old_mode | 0x80);
+    write(fd, MODE1, old_mode | 0x80);
 }
 
-void stop(int motor_id) {
-    ROS_INFO("Stopping motor %d", motor_id);
-    setPin(in1Pin[motor_id], 0);
-    setPin(in2Pin[motor_id], 0);
+void getPWM(int fd, int channel) {
+    int ledval = 0;
+    ledval = read(fd, LED0_OFF_H+4*channel);
+    ledval = ledval & 0xF;
+    ledval <<= 8;
+    ledval += read(fd, LED0_OFF_L+4*channel);
+    ROS_INFO("Read PMW: %d", ledval);
+    
+
+
+/*void setPWM(int fd, int channel, int on, int off) {
+    write(fd, LED0_ON_L+4*channel, on & 0xFF);
+    write(fd, LED0_ON_H+4*channel, on >> 8);
+    write(fd, LED0_OFF_L+4*channel, off & 0xFF);
+    write(fd, LED0_OFF_H+4*channel, off >> 8);
+}*/
 }
 
-void setSpeed(int motor_id, int speed) {
-    ROS_INFO("Setting speed of motor %d to %d", motor_id, speed);
-    if (motor_id < 0 || motor_id > 3) {
-        ROS_ERROR("Invalid motor_id [%d] Expected 0,1,2,3", motor_id);
-        return;
-    }
+void turnOffDriveMotor() {
+    ROS_INFO("Stopping drive motor");
+    setPin(fd_drive, in1Pin_drive, 0);
+    setPin(fd_drive, in2Pin_drive, 0);
+}
+
+void setSpeed(int speed) {
+    ROS_INFO("Setting speed of drive motor to %d", speed);
     if (speed < -255 || speed > 255) {
         ROS_ERROR("Invalid Speed [%d] Expected >=-255 <=255", speed);
         return;
@@ -137,25 +197,46 @@ void setSpeed(int motor_id, int speed) {
 
     if (speed == 0) {
         // RELEASE
-        stop(motor_id);
+        turnOffDriveMotor();
     }
     else if (speed > 0) {
         // FORWARD
-        setPin(in2Pin[motor_id], 0);
-        setPin(in1Pin[motor_id], 1);
-        setPWM(pwmPin[motor_id], 0, speed * 16);
+        setPin(fd_drive, in2Pin_drive, 0);
+        setPin(fd_drive, in1Pin_drive, 1);
+        setPWM(fd_drive, pwmPin_drive, 0, speed * 16);
     }
     else { // => speed < 0
         // BACKWARD
-        setPin(in1Pin[motor_id], 0);
-        setPin(in2Pin[motor_id], 1);
-        setPWM(pwmPin[motor_id], 0, -speed * 16);
+        setPin(fd_drive, in2Pin_drive, 1);
+        setPin(fd_drive, in1Pin_drive, 0);
+        setPWM(fd_drive, pwmPin_drive, 0, -speed * 16);
     }
 }
 
-void turnOffMotors() {
-    for (int i = 0; i < 4; ++i)
-        stop(i);
+void setSteer(int steer) {
+    ROS_INFO("Setting steer motor to %d", steer);
+    /*if (speed < -255 || speed > 255) {
+        ROS_ERROR("Invalid Speed [%d] Expected >=-255 <=255", speed);
+        return;
+    }
+
+    if (speed == 0) {
+        // RELEASE
+        turnOffDriveMotor();
+    }
+    else if (speed > 0) {
+        // FORWARD
+        setPin(fd_drive, in2Pin_drive, 0);
+        setPin(fd_drive, in1Pin_drive, 1);
+        setPWM(fd_drive, pwmPin_drive, 0, speed * 16);
+    }
+    else { // => speed < 0
+        // BACKWARD
+        setPin(fd_drive, in2Pin_drive, 0);
+        setPin(fd_drive, in1Pin_drive, 1);
+        setPWM(fd_drive, pwmPin_drive, 0, -speed * 16);
+    }*/
+    setPWM(fd_steer, pwmPin_steer, 0, steer);
 }
 
 // Receive a twist message corresponding to the desired motor speeds.
@@ -185,8 +266,11 @@ void cmdCallback(const geometry_msgs::Twist::ConstPtr& msg) {
         z = cst_max_twist;
     }
 
-    int valleft=0, valrigth=0;
-    if(x > z){
+    int valdrive=0, valsteer=0;
+
+    valdrive = cst_max_speed * x / cst_max_twist;
+    valdrive *= flagforward;
+    /*if(x > z){
         // linear command is considered
         valleft = valrigth = int(cst_max_speed*x/(cst_max_twist));  // linear speed of all the motors
         if( flagleft == 1 ){
@@ -201,19 +285,38 @@ void cmdCallback(const geometry_msgs::Twist::ConstPtr& msg) {
         // valleft = -int(cst_max_speed*(z-x)/(cst_max_twist))*flagleft;    // angular speed for the right motors
 	valleft = -int(cst_max_speed*(z)/(cst_max_twist))*flagleft;    // angular speed for the right motors
         valrigth = -valleft;                                    // angular speed for the left motors
-    }
+    }*/
 
-    setSpeed(cst_motor_bot_left, -valleft);
-    setSpeed(cst_motor_bot_right, -valrigth);
-    setSpeed(cst_motor_front_left, -valleft);
-    setSpeed(cst_motor_front_right, -valrigth);
+    setSpeed(valdrive);
+    setPWM(fd_steer, 15, 0, 100);
+    getPWM(fd_steer, 15);
+    usleep(5000000);
+    setPWM(fd_steer, 15, 0, 200);
+    getPWM(fd_steer, 15);
+    usleep(5000000);
+    setPWM(fd_steer, 15, 0, 300);
+    getPWM(fd_steer, 15);
+    usleep(5000000);
+    setPWM(fd_steer, 15, 0, 400);
+    getPWM(fd_steer, 15);
+    usleep(5000000);
+    setPWM(fd_steer, 15, 0, 500);
+    getPWM(fd_steer, 15);
+    usleep(5000000);
+    setPWM(fd_steer, 15, 0, 0);
+    getPWM(fd_steer, 15);
+
+    
+
+    getPWM(fd_steer, 15);
 }
 
 int main(int argc, char **argv) {
     // Setup.
     init_hat();
-    setPWMFreq(1600);
-    atexit(turnOffMotors);
+    setPWMFreq(fd_drive, 1600);
+    // setPWMFreq(fd_steer, 50);
+    atexit(turnOffDriveMotor);
 
     // Start ROS node stuff.
     ros::init(argc, argv, "motor_hat_twist_node");
@@ -222,10 +325,8 @@ int main(int argc, char **argv) {
     // Get parameters so we can change these later.
     // this allows to use the launch file parameters!
     node.param<std::string>("/motor_hat_twist_node/topic_name", cst_topic_name, "/default/cmd_vel");
-    node.param<int>("/motor_hat_twist_node/motor_bot_left", cst_motor_bot_left, 0);
-    node.param<int>("/motor_hat_twist_node/motor_bot_right", cst_motor_bot_right, 1);
-    node.param<int>("/motor_hat_twist_node/motor_front_left", cst_motor_front_left, 3);
-    node.param<int>("/motor_hat_twist_node/motor_front_right", cst_motor_front_right, 2);
+    node.param<int>("/motor_hat_twist_node/motor_drive", cst_motor_drive, 0);
+    node.param<int>("/motor_hat_twist_node/motor_steer", cst_motor_steer, 1);
     node.param<int>("/motor_hat_twist_node/max_speed", cst_max_speed, 100);
     node.param<double>("/motor_hat_twist_node/max_twist", cst_max_twist, 1);
 
@@ -237,3 +338,4 @@ int main(int argc, char **argv) {
     ros::spin();
     return 0;
 }
+
